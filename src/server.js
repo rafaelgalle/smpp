fcb1678f00851c59c4f5bfb5a1aaa5cd2e97108c
RafaelGalle <rafaelgalle1@gmail.com>
2022-08-28 22:30:58 -0300

    Adding server files

---
+++ src/server.js
@@ -0,0 +1,45 @@
+var smpp = require('smpp')
+var server = smpp.createServer({
+	debug: true
+}, function(session) {
+
+	session.on('error', function (err) {
+		console.error(err)
+  })
+
+	session.on('bind_transceiver', function(pdu) {
+		session.pause()
+    var checkAsyncUserPass = function (user, pwd, onComplete) {
+      setTimeout(function () {
+        if (user === "FAKE_USER" && pwd === "FAKE_PASSWORD") {
+          onComplete()
+        } else {
+          onComplete("invalid user and password combination")
+        }
+      }, 25)
+    }
+		checkAsyncUserPass(pdu.system_id, pdu.password, function(err) {
+			if (err) {
+				session.send(pdu.response({
+					command_status: smpp.ESME_RBINDFAIL
+				}))
+				session.close()
+				return
+			}
+			session.send(pdu.response())
+			session.resume()
+		})
+	})
+
+  session.on('submit_sm', function(pdu) {
+    session.send(pdu.response({
+      message_id: fakeId()
+    }))
+  })
+})
+
+server.listen(2775)
+
+function fakeId(){
+	return 'f' + Math.floor((Math.random() * 100) + 1) + 'a' + Math.floor((Math.random() * 100) + 1) + 'b' + Math.floor((Math.random() * 100) + 1)
+}
